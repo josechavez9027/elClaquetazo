ce60bea22dd3353008a78692f24f6859bae221e7
josec <josechf1395@gmail.com>
2026-05-12 20:38:35 -0600

    third commit

--- routes/api.php
+++ routes/api.php
@@ -5,11 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MovieController;
 use App\Models\Movie;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
 Route::get('/movies', [MovieController::class, 'index']);
 Route::get('/movies/{id}', [MovieController::class, 'show']);
-Route::post('/movies/post', [MovieController::class, 'store']);
-Route::put('/movies/put/{id}', [MovieController::class, 'update']);
+Route::post('/movies', [MovieController::class, 'store']);
+Route::put('/movies/{id}', [MovieController::class, 'update']);
 Route::delete('/movies/delete/{id}', [MovieController::class, 'destroy']);
