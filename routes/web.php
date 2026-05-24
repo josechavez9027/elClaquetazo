aa9836ef6e360ee8552d513172cc73b63e482d06
josec <josechf1395@gmail.com>
2026-05-23 23:07:59 -0600

    routes9

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     $path = public_path('angular/browser/index.html');
-    
     if (file_exists($path)) {
         return file_get_contents($path);
     }
-    
-    return abort(404, 'No se encontró el build de Angular en public/angular/browser');
-})->where('any', '.*');
+    return abort(404);
+})->where('any', '^(?!api).*$');
