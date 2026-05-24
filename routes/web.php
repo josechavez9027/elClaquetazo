4995974af0226c3c244c58fc5794ee70312dd9f0
josec <josechf1395@gmail.com>
2026-05-24 00:59:49 -0600

    angularjson13

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/{any}', function () {
+//     $path = public_path('angular/browser/index.html');
+//     if (file_exists($path)) {
+//         return file_get_contents($path);
+//     }
+//     return abort(404);
+// })->where('any', '^(?!api).*$');
+
 Route::get('/{any}', function () {
-    $path = public_path('angular/browser/index.html');
-    if (file_exists($path)) {
-        return file_get_contents($path);
-    }
-    return abort(404);
-})->where('any', '^(?!api).*$');
+    return response(
+        file_get_contents(public_path('angular/browser/index.html')), 200
+    )->header('Content-Type', 'text/html');
+})->where('any', '^(?!api)(?!.*\.(js|css|ico|png|jpg|svg|woff|woff2|ttf|json|map)).*$');
