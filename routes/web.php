1f55f7178638ef5832954b0a5981ba61c45aa47d
josec <josechf1395@gmail.com>
2026-05-23 22:17:47 -0600

    routes3

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
-    $path = public_path('angular/index.html');
+    $path = public_path('angular/browser/index.html');
     
     if (File::exists($path)) {
         return file_get_contents($path);
     }
-
+    
     return response()->json(['error' => 'Frontend no compilado o ruta no encontrada'], 404);
 })->where('any', '^(?!api).*$');
