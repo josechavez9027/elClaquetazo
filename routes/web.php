4abbb02d9c42d71e75de80cd93339adb55c96fd7
josec <josechf1395@gmail.com>
2026-05-24 01:15:58 -0600

    routes3

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/{any}', function () {
-//     $path = public_path('angular/browser/index.html');
-//     if (file_exists($path)) {
-//         return file_get_contents($path);
-//     }
-//     return abort(404);
-// })->where('any', '^(?!api).*$');
-
 Route::get('/{any}', function () {
-    return response(
-        file_get_contents(public_path('angular/browser/index.html')), 200
-    )->header('Content-Type', 'text/html');
-})->where('any', '^(?!api)(?!.*\.(js|css|ico|png|jpg|svg|woff|woff2|ttf|json|map)).*$');
+    $path = public_path('angular/browser/index.html');
+    if (file_exists($path)) {
+        return file_get_contents($path);
+    }
+    return abort(404);
+})->where('any', '^(?!api).*$');
+
+// Route::get('/{any}', function () {
+//     return response(
+//         file_get_contents(public_path('angular/browser/index.html')), 200
+//     )->header('Content-Type', 'text/html');
+// })->where('any', '^(?!api)(?!.*\.(js|css|ico|png|jpg|svg|woff|woff2|ttf|json|map)).*$');
