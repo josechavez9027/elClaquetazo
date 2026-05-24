beff02012a0de59d17b0480a367ec558342f9cb4
josec <josechf1395@gmail.com>
2026-05-24 11:53:55 -0600

    Limpieza de cache3

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/{any}', function () {
+    $path = public_path('angular/browser/index.html');
+    if (file_exists($path)) {
+        return file_get_contents($path);
+    }
+    return abort(404);
+})->where('any', '^(?!api).*$');
